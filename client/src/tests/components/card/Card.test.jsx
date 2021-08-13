53a32b0289b5bd551e8527746f2d4c0d6eb83264
martin <martin.milone2011@gmail.com>
2021-08-13 17:54:06 -0300

    faltan tests

---
+++ client/src/tests/components/card/Card.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { shallow } from 'enzyme';
+
+import Card from '../../../components/card/Card';
+
+describe('Pruebas en el componente card', () => {
+
+    describe('debe renderiar un h3 con el nombre de la raza', () => {
+        
+        const wrapper = shallow()
+    })
+    
+
+
+    
+})
